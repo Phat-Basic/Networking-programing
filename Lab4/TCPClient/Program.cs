b5efc2b63a55aa0349a7ee67b7a6e983d972ef1d
Phát Handsome <phattranlikedata@gmail.com>
2024-09-20 09:04:15 +0700

    Add files via upload
    Update lab 4

---
+++ Lab4/TCPClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace TCPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            string input, stringData;
+            TcpClient server;
+            try{
+                server = new TcpClient("127.0.0.1", 9000);
+            }
+            catch (SocketException) {
+                Console.WriteLine("Unable to connecting sever");
+                return;
+            }
+            //
+            NetworkStream ns=server.GetStream();
+            //
+            int recv = ns.Read(data, 0, data.Length);
+            stringData=Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine(stringData);
+            while (true) { 
+                input =Console.ReadLine();
+                if(input=="exit")
+                { break; }
+                ns.Write(Encoding.ASCII.GetBytes(input),0, input.Length);
+                ns.Flush();
+                data = new byte[1024];
+                recv= ns.Read(data, 0, data.Length);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine(stringData);
+            }
+            Console.WriteLine("Disconnecting from server...");
+            ns.Close();
+            server.Close();
+        }
+    }
+}
---
+++ Lab4/UDPServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            IPEndPoint ipep= new IPEndPoint(IPAddress.Any, 9000);
+            UdpClient newsock = new UdpClient(ipep);
+            Console.WriteLine("Waiting for client...");
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            data= newsock.Receive(ref sender);
+            Console.WriteLine("Message received from {0}:", sender.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data,0 , data.Length));
+            string welcome = "Welcome to my server ";
+            data=Encoding.ASCII.GetBytes(welcome);
+            newsock.Send(data, data.Length, sender);
+            while (true) { 
+                data = newsock.Receive(ref sender);
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+                newsock.Send(data,data.Length, sender);
+            }
+
+        }
+    }
+}
